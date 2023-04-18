bd31217579d8453979372586f7160aafc51c1ae5
Jake Whitworth <92970259+jdubindaclub@users.noreply.github.com>
2023-04-17 20:14:16 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -43,9 +43,10 @@ for column, column_properties in schema['column_info'].items():
         options[column] = st.sidebar.selectbox(column, column_properties['values'])
     
 #load model and encoder
-model_path = os.path.join('..', 'models', 'experiment_1', 'xgb.pkl')
-with open(model_path, 'rb') as f:
-    model = pickle.load(f)
+# model_path = os.path.join('..', 'models', 'experiment_1', 'xgb.pkl')
+# with open(model_path, 'rb') as f:
+#     model = pickle.load(f)
+model = pickle.load(open('xgbclass','rb'))
 
 encoder_path = os.path.join('..', 'models', 'experiment_1', 'encoder.pkl')
 with open(encoder_path, 'rb') as f:
